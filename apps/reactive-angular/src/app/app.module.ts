9e19c8b6f4ce8ee5f6dce25a92a600d6ac2aea1c
Filip Voska <filip.voska@infinum.com>
2022-05-12 21:28:59 +0200

    Solution

--- apps/reactive-angular/src/app/app.module.ts
+++ apps/reactive-angular/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
